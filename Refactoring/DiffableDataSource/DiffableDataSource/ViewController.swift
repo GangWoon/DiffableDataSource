a33a7bed0c852706f2b0c938a027439bae2f8422
GangWoon <gangwoon2018@gmail.com>
2021-07-22 02:00:45 +0900

    Init refactoring project

---
+++ Refactoring/DiffableDataSource/DiffableDataSource/ViewController.swift
@@ -0,0 +1,17 @@
+//
+//  ViewController.swift
+//  DiffableDataSource
+//
+//  Created by Cloud on 2021/07/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .white
+    }
+}
+
